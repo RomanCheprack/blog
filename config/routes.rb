cad6e29a824f3ca15b2feb0bc02f591856c30192
RomanCheprack <romancheprack@gmail.com>
2018-09-12 18:43:09 +0300

    adding routes and pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+  get 'pages/contact'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'articles#index'
+  root to: 'pages#home'
   resources :articles, except: :index
 end
